d5bee149164b88474e5786541ebbcd9de84e4c56
prantikmedhi <140103052+prantikmedhi@users.noreply.github.com>
2026-09-20 12:04:30 +0530

    player killzone animation

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -5,12 +5,15 @@ var direction = 1
 
 @onready var raycast_right: RayCast2D = $RaycastRight
 @onready var raycast_left: RayCast2D = $RaycastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta): 
 	if raycast_right.is_colliding():
 		direction = -1
+		animated_sprite.flip_h = true
 	if raycast_left.is_colliding():
 		direction = 1
+		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
